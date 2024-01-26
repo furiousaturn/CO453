b08706007d2e3e75f325c4dd86e6b3bc2836d199
amstephenson <amstephenson@burnley.ac.uk>
2024-01-26 11:23:42 +0000

    changed colour green

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleAppProject
     {
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine();
             Console.WriteLine(" =================================================");
