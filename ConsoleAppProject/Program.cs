219ff93a0d6e29a851962bdf973987b7d26f32b0
Andrew Stephenson <amstephenson@burnley.ac.uk>
2024-01-26 05:51:04 +0000

    Added my name to Program.cs

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Andrew Stephenson 26/01/2023
     /// </summary>
     public static class Program
     {
