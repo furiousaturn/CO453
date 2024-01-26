c2e64e22e1ddcd03ef8c9963a5a57e5311fdab36
amstephenson <amstephenson@burnley.ac.uk>
2024-01-26 11:42:24 +0000

    structure distance converter

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,45 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Converts between different distances
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Andrew Stephenson version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+        
+        ///<summary>
+        ///</summary>
+        public void Run()
+        {
+            
+        }
+
+        ///<summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        ///</summary>
+        private void InputMiles()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        private void OutputFeet()
+        {
+
+        }
     }
+
 }
